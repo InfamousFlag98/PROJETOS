1a393d8239505828e759561eeb7af69bbb1a22ba
InfamousFlag98 <guimajr98@gmail.com>
2025-10-30 15:57:50 -0300

    "updates 2"

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from modelos.alura import Restaurante
 restaurante_1 = Restaurante('praça', 'gourmet')
 restaurante_2 = Restaurante('mexican food', 'mexicano')
 restaurante_3 = Restaurante('japa', 'japonesa')
+restaurante_1.receber_avaliacao('Gui', 10)
+restaurante_1.receber_avaliacao('Lais', 8)
+restaurante_1.receber_avaliacao('Emy', 2)
 
 restaurante_2.alternar_estado()
 
